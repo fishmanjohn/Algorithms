164267baa31c6030722ab2b621a124e48cd05ece
fishmanjohn <john@emmaandjohnbyvan.com>
2020-04-15 19:53:54 -0700

    fing finally

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,10 +2,31 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+  for i in range(len(prices)):
+    current_index = i 
+    buy_price = current_index 
+    sell_price = current_index
+    profit = None
+    for x in range(buy_price, len(prices) - 1):
+      if prices[buy_price] > prices[x]:
+        buy_price = x
+        sell_price = x 
+      elif prices[sell_price] < prices[x]:
+        sell_price = x 
+      
+    if buy_price == sell_price:
+      sell_price = sell_price + 1      
+    profit = prices[sell_price] - prices[buy_price]
+    return profit
+   
+
+    
 
 
+
+print(find_max_profit([1050, 270, 1540, 3800, 2]))
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
   parser = argparse.ArgumentParser(description='Find max profit from prices.')
