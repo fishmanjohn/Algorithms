793c8c83d4b91083909fca67b93d7d744ba8acd0
fishmanjohn <john@emmaandjohnbyvan.com>
2020-04-16 07:03:28 -0700

    recipies works

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,9 +3,29 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  batch_counter = 0
+  while True:
+    if len(recipe) != len(ingredients):
+      return batch_counter
+    elif len(recipe) and len(ingredients) == 1:
+      for i in ingredients:
+        if recipe[i] > ingredients[i]:
+          return batch_counter
+        else: 
+          ingredients[i] = ingredients[i] - recipe[i]
+          batch_counter = batch_counter + 1 
+    else:
+      for i in ingredients:
+        if recipe[i] > ingredients[i]:
+          batch_counter = batch_counter // 3
+          return batch_counter
+        else: 
+          ingredients[i] = ingredients[i] - recipe[i]
+          batch_counter = batch_counter + 1 
+          
+#if 
 
-
+print(recipe_batches({ 'milk': 2, 'sugar': 40, 'butter': 20 }, { 'milk': 5, 'sugar': 120, 'butter': 500 }))
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
